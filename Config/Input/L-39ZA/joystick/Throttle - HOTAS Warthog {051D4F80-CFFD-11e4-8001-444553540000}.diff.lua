bcdb7a9462687009f4ea5d6fe2888bea35e6ac44
Vaclav Danek <danvac@seznam.cz>
2018-07-08 18:16:06 +0200

    L-39ZA: added config for throttle

---
+++ Config/Input/L-39ZA/joystick/Throttle - HOTAS Warthog {051D4F80-CFFD-11e4-8001-444553540000}.diff.lua
@@ -0,0 +1,45 @@
+local diff = {
+	["keyDiffs"] = {
+		["d72pnilu72cdnilvd1vpnilvu4"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN32",
+				},
+			},
+			["name"] = "Flaps Flight Position",
+		},
+		["d72pnilu72cdnilvd2vpnilvu5"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN22",
+				},
+			},
+			["name"] = "Flaps TakeOff Position",
+		},
+		["d72pnilu72cdnilvd3vpnilvu6"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN23",
+				},
+			},
+			["name"] = "Flaps Landing Position",
+		},
+		["dnilp3010unilcd1vdnilvp-0.7vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+			["name"] = "ASP-3NMU Gunsight Target Distance, Decrease",
+		},
+		["dnilp3010unilcd1vdnilvp0.7vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+			["name"] = "ASP-3NMU Gunsight Target Distance, Increase",
+		},
+	},
+}
+return diff
