03ed97192ef133d7ff9b3a8d4d322f47e88ef277
Vaclav Danek <danvac@seznam.cz>
2018-07-08 13:39:02 +0200

    L-39C: flaps controls

---
+++ Config/Input/L-39C/joystick/Throttle - HOTAS Warthog {051D4F80-CFFD-11e4-8001-444553540000}.diff.lua
@@ -0,0 +1,35 @@
+local diff = {
+	["keyDiffs"] = {
+		["d72pnilu72cdnilvd1vpnilvu4"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN32",
+				},
+			},
+			["name"] = "Flaps Flight Position",
+		},
+		["d72pnilu72cdnilvd2vpnilvu5"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+				[2] = {
+					["key"] = "JOY_BTN22",
+				},
+			},
+			["name"] = "Flaps TakeOff Position",
+		},
+		["d72pnilu72cdnilvd3vpnilvu6"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+				[2] = {
+					["key"] = "JOY_BTN23",
+				},
+			},
+			["name"] = "Flaps Landing Position",
+		},
+	},
+}
+return diff
