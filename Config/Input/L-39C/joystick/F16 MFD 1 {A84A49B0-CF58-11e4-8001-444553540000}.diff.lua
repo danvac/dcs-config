c36a45664b2c7496e14804c75cc0bc98d21d5103
Vaclav Danek <danvac@seznam.cz>
2018-07-08 14:16:17 +0200

    L-39C: added F-16 MFD 1

---
+++ Config/Input/L-39C/joystick/F16 MFD 1 {A84A49B0-CF58-11e4-8001-444553540000}.diff.lua
@@ -0,0 +1,119 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2001cdnil"] = {
+			["name"] = "Pitch",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Y",
+				},
+			},
+		},
+		["a2002cdnil"] = {
+			["name"] = "Roll",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_X",
+				},
+			},
+		},
+		["a2003cdnil"] = {
+			["name"] = "Rudder",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_RZ",
+				},
+			},
+		},
+		["a2004cdnil"] = {
+			["name"] = "Thrust",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Z",
+				},
+			},
+		},
+	},
+	["keyDiffs"] = {
+		["d3022pnilunilcd3vd1vpnilvunil"] = {
+			["name"] = "Weapon Fire Button Safety",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+		},
+		["d3028pnilunilcd3vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+			["name"] = "CB Armament System Power Switch - ON/OFF",
+		},
+		["d3029pnilunilcd3vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "CB UB-16 Rocket Firing Control Circuit Power Switch - ON/OFF",
+		},
+		["d3030pnilunilcd3vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "CB ASP-FKP (Gunsight and Gun Camera) Power Switch - ON/OFF",
+		},
+		["d3031pnilunilcd3vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "CB Missile Seeker Heating Circuit Power Switch - ON/OFF",
+		},
+		["d3032pnilunilcd3vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "CB Missile Seeker Glowing Circuit Power Switch - ON/OFF",
+		},
+		["d3039pnilunilcd3vd0.1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "Rockets Firing Mode Selector Switch - AUT.",
+		},
+		["d3039pnilunilcd3vd0.2vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "Rockets Firing Mode Selector Switch - 2RS",
+		},
+		["d3039pnilunilcd3vd0.3vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "Rockets Firing Mode Selector Switch - 4RS",
+		},
+		["d84pnilu85cdnilvdnilvpnilvunil"] = {
+			["name"] = "Weapon Fire Button",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+		},
+	},
+}
+return diff
