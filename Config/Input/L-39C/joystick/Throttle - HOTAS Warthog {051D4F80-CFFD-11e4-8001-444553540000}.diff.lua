61f81c0ac673e028b5f7f2421f9c3404cfe3b433
Vaclav Danek <danvac@seznam.cz>
2018-07-08 18:16:39 +0200

    L-39C: removed flaps from china switch

--- Config/Input/L-39C/joystick/Throttle - HOTAS Warthog {051D4F80-CFFD-11e4-8001-444553540000}.diff.lua
+++ Config/Input/L-39C/joystick/Throttle - HOTAS Warthog {051D4F80-CFFD-11e4-8001-444553540000}.diff.lua
@@ -11,9 +11,6 @@ local diff = {
 		["d72pnilu72cdnilvd2vpnilvu5"] = {
 			["added"] = {
 				[1] = {
-					["key"] = "JOY_BTN11",
-				},
-				[2] = {
 					["key"] = "JOY_BTN22",
 				},
 			},
@@ -22,9 +19,6 @@ local diff = {
 		["d72pnilu72cdnilvd3vpnilvu6"] = {
 			["added"] = {
 				[1] = {
-					["key"] = "JOY_BTN12",
-				},
-				[2] = {
 					["key"] = "JOY_BTN23",
 				},
 			},
