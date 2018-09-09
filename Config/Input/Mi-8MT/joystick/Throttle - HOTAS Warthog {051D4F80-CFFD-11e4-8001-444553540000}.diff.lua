c7fa134a257e9936feef22dcba2e01db9a83cfa5
Vaclav Danek <danvac@seznam.cz>
2018-09-09 12:05:26 +0200

    Mi-8MT: initial input config

---
+++ Config/Input/Mi-8MT/joystick/Throttle - HOTAS Warthog {051D4F80-CFFD-11e4-8001-444553540000}.diff.lua
@@ -0,0 +1,161 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2087cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["invert"] = true,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_Z",
+				},
+			},
+			["name"] = "Flight Control Collective",
+		},
+	},
+	["keyDiffs"] = {
+		["d3003pnilu3003cd36vd0.1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+			["name"] = "SPU-7 Radio Source Select Rotary - Jadro-1A (Warthog)",
+		},
+		["d3003pnilu3003cd36vd0.2vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "SPU-7 Radio Source Select Rotary - R-828 (Warthog)",
+		},
+		["d3003pnilu3003cd36vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "SPU-7 Radio Source Select Rotary - R-863 (Warthog)",
+		},
+		["d3012pnilu3012cd9vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "Nav. Lights Switch - DIM (Warthog)",
+		},
+		["d3012pnilu3012cd9vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "Nav. Lights Switch - BRIGHT (Warthog)",
+		},
+		["d3015pnilu3015cd9vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN25",
+				},
+			},
+			["name"] = "Strobe Light Switch (Warthog)",
+		},
+		["d3017pnilu3017cd9vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN24",
+				},
+			},
+			["name"] = "Taxi Light Power Switch (Warthog)",
+		},
+		["d3018pnilu3018cd9vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN28",
+				},
+			},
+			["name"] = "Left Headlight Power Switch - RETRACT (Warthog)",
+		},
+		["d3018pnilu3018cd9vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN27",
+				},
+			},
+			["name"] = "Left Headlight Power Switch - LIGHT (Warthog)",
+		},
+		["d3021pnilu3021cd9vd1vpnilvu0"] = {
+			["name"] = "Right Headlight - Down",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+		},
+		["d3021pnilu3021cd9vd2vpnilvu0"] = {
+			["name"] = "Right Headlight - Up",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+		},
+		["d3021pnilu3021cd9vd3vpnilvu0"] = {
+			["name"] = "Right Headlight - Left",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+		},
+		["d3021pnilu3021cd9vd4vpnilvu0"] = {
+			["name"] = "Right Headlight - Right",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+		},
+		["d600pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+				},
+			},
+			["name"] = "Cut-off valve left engine",
+		},
+		["d601pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN17",
+				},
+			},
+			["name"] = "Cut-off valve right engine",
+		},
+		["d853pnilu853cdnilvd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "Readjust Free Turbine RPM Switch - Increase",
+		},
+		["d854pnilu854cdnilvd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "Readjust Free Turbine RPM Switch - Decrease",
+		},
+	},
+}
+return diff
