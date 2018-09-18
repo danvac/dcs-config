e1ae89ca6679a578e96875aee2ecd9652d7ec095
Vaclav Danek <danvac@seznam.cz>
2018-09-18 20:47:48 +0200

    NS430: added F16 MFD 2 diff

---
+++ Config/Input/NS430/joystick/F16 MFD 2 {A84A49B0-CF58-11e4-8002-444553540000}.diff.lua
@@ -0,0 +1,165 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3008pnilunilcd257vd-0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN28",
+				},
+			},
+			["name"] = "Left big knob Decrease (down)",
+		},
+		["d3008pnilunilcd257vd0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN27",
+				},
+			},
+			["name"] = "Left big knob Increase (down)",
+		},
+		["d3011pnilunilcd257vd-0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN26",
+				},
+			},
+			["name"] = "Left small knob Decrease (down)",
+		},
+		["d3011pnilunilcd257vd0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN25",
+				},
+			},
+			["name"] = "Left small knob Increase (down)",
+		},
+		["d3014pnilu3035cd257vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+			["name"] = "CDI",
+		},
+		["d3015pnilu3036cd257vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "OBS",
+		},
+		["d3016pnilu3037cd257vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "MSG",
+		},
+		["d3017pnilu3038cd257vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "FPL",
+		},
+		["d3018pnilu3039cd257vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "PROC",
+		},
+		["d3019pnilu3040cd257vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "RNG-UP",
+		},
+		["d3020pnilu3041cd257vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "RNG-DOWN",
+		},
+		["d3021pnilu3042cd257vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+			["name"] = "DIRECT-TO",
+		},
+		["d3022pnilu3043cd257vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "MENU",
+		},
+		["d3023pnilu3044cd257vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "CLR",
+		},
+		["d3024pnilu3045cd257vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "ENT",
+		},
+		["d3026pnilunilcd257vd-0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN22",
+				},
+			},
+			["name"] = "Right big knob Decrease (down)",
+		},
+		["d3026pnilunilcd257vd0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN21",
+				},
+			},
+			["name"] = "Right big knob Increase (down)",
+		},
+		["d3027pnilu3046cd257vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "Right small knob push",
+		},
+		["d3029pnilunilcd257vd-0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN24",
+				},
+			},
+			["name"] = "Right small knob Decrease (down)",
+		},
+		["d3029pnilunilcd257vd0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN23",
+				},
+			},
+			["name"] = "Right small knob Increase (down)",
+		},
+	},
+}
+return diff
